16275a8e34c4c5fbaf3dea38b1934309ede2f8a4
syahidhzblh <syahidhizbullah17@gmail.com>
2024-01-09 01:01:26 +0700

    Add files via upload

---
+++ sql_injection_test.go
@@ -0,0 +1,40 @@
+package golang_database
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestSqlInjection(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	username := "admin';#"
+	password := "admin"
+
+	// For Prevent from SQL Injection
+	// Replace varibale username & password with (?) for adding as parameter on QueryContext
+	script := "SELECT username FROM user WHERE username = ? AND password = ? LIMIT 1;"
+
+	fmt.Println(script)
+
+	rows, err := db.QueryContext(ctx, script, username, password)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	if rows.Next() {
+		var username string
+		rows.Scan(&username)
+		fmt.Println("Success Login", username)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		fmt.Println("Failed Login")
+	}
+}
--- sql_test.go
+++ sql_test.go
@@ -2,8 +2,10 @@ package golang_database
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestExecSql(t *testing.T) {
@@ -28,7 +30,7 @@ func TestQuerySql(t *testing.T) {
 	ctx := context.Background()
 
 	script := "SELECT id, name FROM customer;"
-	rows, err := db.QueryContext(ctx, script) //ExecContext can use for INSERT, UPDATE, DELETE (DML)
+	rows, err := db.QueryContext(ctx, script)
 	if err != nil {
 		panic(err)
 	}
@@ -43,4 +45,66 @@ func TestQuerySql(t *testing.T) {
 		fmt.Println("Id: ", id)
 		fmt.Println("Name: ", name)
 	}
+}
+
+func TestQueryComplex(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "SELECT id, name, email, balance, rating, birth_date, married, created_at FROM customer;"
+	rows, err := db.QueryContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var (
+			id, name   string
+			email      sql.NullString
+			balance    int32
+			rating     float64
+			created_at time.Time
+			birth_date sql.NullTime
+			married    bool
+		)
+
+		err = rows.Scan(&id, &name, &email, &balance, &rating, &birth_date, &married, &created_at)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("===========")
+		fmt.Println("Id:", id)
+		fmt.Println("Name:", name)
+		if email.Valid { //Check for Null Data
+			fmt.Println("Email:", email.String)
+		}
+		fmt.Println("Balance:", balance)
+		fmt.Println("Rating:", rating)
+		if birth_date.Valid {
+			fmt.Println("Birth Date:", birth_date.Time)
+		}
+		fmt.Println("Married:", married)
+		fmt.Println("Created At:", created_at)
+	}
+}
+
+func TestExecSqlParameter(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	username := "syahid"
+	password := "syahid"
+
+	script := "INSERT INTO user(username,password) VALUES (?,?)"
+	_, err := db.ExecContext(ctx, script, username, password)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Success Insert new user")
 }
