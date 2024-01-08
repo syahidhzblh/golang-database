592f7550b071443c8f887a6d871c9f6e1a774925
syahidhzblh <syahidhizbullah17@gmail.com>
2024-01-08 23:24:46 +0700

    Add files via upload

---
+++ sql_test.go
@@ -0,0 +1,46 @@
+package golang_database
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestExecSql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "INSERT INTO customer(id, name) VALUES('hisbul', 'Hisbul');"
+	_, err := db.ExecContext(ctx, script) //ExecContext can use for INSERT, UPDATE, DELETE (DML)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Success insert new customer")
+}
+
+func TestQuerySql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "SELECT id, name FROM customer;"
+	rows, err := db.QueryContext(ctx, script) //ExecContext can use for INSERT, UPDATE, DELETE (DML)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name string
+		err = rows.Scan(&id, &name)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Id: ", id)
+		fmt.Println("Name: ", name)
+	}
+}
